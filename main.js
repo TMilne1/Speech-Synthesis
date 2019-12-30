66f2f736c1b860449b37ea62f332307e11f9c5b1
tmilne1 <tmilne1@gmail.com>
2019-12-30 14:17:34 -0500

    event listers stopped firing in safari, and fire fox

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const stopButton = document.querySelector('#stop');
 const textbox = document.querySelector('textarea')
 
 
-function speak(){
-    speechSynthesis.cancel()
+function toggle(startover = true){
     msg.text = textbox.value // value is better than textcontent because it updates as you change the text
-    speechSynthesis.speak(msg)
+    speechSynthesis.cancel()
+    if (startover){speechSynthesis.speak(msg)}
 }
 
 function getvoices(){
-    voices = this.getVoices();
+    voices = speechSynthesis.getVoices();
     voicesDropdown.innerHTML = voices
         .map((voice)=>{
             return `<option value ="${voice.name}">${voice.name} ${voice.lang}</option>`})
@@ -22,8 +22,19 @@ function getvoices(){
         //msg.voice = voices[0] ---- not needed will default to first voice.
 }
 
+function changeVoice(event){
+    let newVoice = voices.filter((value) =>{
+        return value.voiceURI == event.target.value})
+    msg.voice = newVoice[0]
+    toggle()
+    //alot less clumsy
+    // voices.find(voice => voice.name == this.value)
+} 
 
-speechSynthesis.addEventListener('voiceschanged', getvoices)
-speakButton.addEventListener('click', speak)
-stopButton.addEventListener('click', ()=>speechSynthesis.cancel())
+
+
+speakButton.addEventListener('click', toggle)
+stopButton.addEventListener('click', ()=>toggle(false))
+voicesDropdown.addEventListener('change',changeVoice)
+speechSynthesis.addEventListener('voiceschanged', getvoices) // voiceschanged event reandomly stopped wprkng in firefox
 
