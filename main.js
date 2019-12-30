00014edeea887d386d53cbb29756dafebd8ff297
tmilne1 <tmilne1@gmail.com>
2019-12-29 22:15:48 -0500

    start and stop speech with buttons

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+const textbox = document.querySelector('textarea')
 
 
 function speak(){
+    speechSynthesis.cancel()
     msg.text = textbox.value // value is better than textcontent because it updates as you change the text
+    speechSynthesis.speak(msg)
 }
 
 function getvoices(){
@@ -16,11 +19,11 @@ function getvoices(){
         .map((voice)=>{
             return `<option value ="${voice.name}">${voice.name} ${voice.lang}</option>`})
         .join(" ")
-        msg.voice = voices[0]
+        //msg.voice = voices[0] ---- not needed will default to first voice.
 }
 
 
 speechSynthesis.addEventListener('voiceschanged', getvoices)
-
 speakButton.addEventListener('click', speak)
+stopButton.addEventListener('click', ()=>speechSynthesis.cancel())
 
