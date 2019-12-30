fb5b3199cccba0d06ccb547c5fd4977086b71b2b
tmilne1 <tmilne1@gmail.com>
2019-12-29 21:57:55 -0500

    added speach synthesis, populated voices

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+
+function speak(){
+    msg.text = textbox.value // value is better than textcontent because it updates as you change the text
+}
+
+function getvoices(){
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+        .map((voice)=>{
+            return `<option value ="${voice.name}">${voice.name} ${voice.lang}</option>`})
+        .join(" ")
+        msg.voice = voices[0]
+}
+
+
+speechSynthesis.addEventListener('voiceschanged', getvoices)
+
+speakButton.addEventListener('click', speak)
+
